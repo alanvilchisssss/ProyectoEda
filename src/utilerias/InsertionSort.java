377582397524d0c239804aadbd0aba951e5a64c2
Alan Gael Vilchis Testas (github:alanvilchisssss) <alanvilchis07@gmail.com>
2024-09-15 00:45:48 -0600

    InsertionSort agregado

--- src/utilerias/InsertionSort.java
+++ src/utilerias/InsertionSort.java
@@ -2,16 +2,23 @@ package utilerias;
 
 public class InsertionSort{  
     
-    public static void insertionSort(int array[]) {  
+    public static int insertionSort(int array[]) {  
         int n = array.length;  
+        int comparaciones=0;
+        int intercambios=0;
+        int insercion=0;
         for (int j = 1; j < n; j++) {  
             int key = array[j];  
             int i = j-1;  
             while ( (i > -1) && ( array [i] > key ) ) {  
+                comparaciones++;
                 array [i+1] = array [i];  
                 i--;  
+                intercambios++;
             }  
             array[i+1] = key;  
+            insercion++;
         }  
+        return insercion+intercambios+comparaciones;
     } 
 }
