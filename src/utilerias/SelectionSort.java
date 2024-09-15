39c71be80e63ea895f0e8e86817409b09d1c8b4a
Alan Gael Vilchis Testas (github:alanvilchisssss) <alanvilchis07@gmail.com>
2024-09-15 00:54:11 -0600

    SelectionSort

--- src/utilerias/SelectionSort.java
+++ src/utilerias/SelectionSort.java
@@ -1,20 +1,27 @@
 package utilerias;
 public class SelectionSort {  
    
-    public void selectionSort(int[] arr){  
+    public static int selectionSort(int[] arr){  
         int n = arr.length;
+        int comparaciones=0;
+        int intercambio=0;
+        int inserciones=0;
         for (int i = 0; i < n - 1; i++){  
             int min = i;  
             for (int j = i + 1; j < n; j++){  
-                if (arr[j] < arr[min]){  
+                if (arr[j] < arr[min]){
+                    comparaciones++;  
                     min = j;  
                 }                      
             }  
             
             Utilerias.swap(arr, i,min);
-              
+             intercambio++;
+             inserciones++; 
                  
         }  
+        return intercambio+inserciones+comparaciones;
     }  
+ 
 
 }  
